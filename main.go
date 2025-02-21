4ec0579a80a9bd1607cca6f426d48968d3f7c0b2
Nick Neal <[email protected]>
2025-02-20 22:04:19 -0500

    doc: added subcommand usage

--- main.go
+++ main.go
@@ -232,9 +232,9 @@ func printHelp() {
 func printCommandHelp(command string) {
 	switch command {
 	case "lb":
-		fmt.Printf("Usage: %v %v { list | ( new | remove ) <cluster> | <cluster> ( add | del | move | restore | status ) ARGS... }", os.Args[0], command)
+		fmt.Printf("Usage: %v %v { list | ( new | remove ) <cluster> | <cluster> ( add | del | move | restore | status ) ARGS... }\n\n", os.Args[0], command)
 	case "proxy":
-		fmt.Printf("Usage: %v %v { list ARGS... | ( new | remove | enable | disable ) <cluster> ARGS... }", os.Args[0], command)
+		fmt.Printf("Usage: %v %v { list ARGS... | ( new | remove | enable | disable ) <cluster> ARGS... }\n\n", os.Args[0], command)
 	default:
 		printHelp()
 	}
