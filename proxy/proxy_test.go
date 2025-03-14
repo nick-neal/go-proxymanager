18ec4986efba5ab609f81acf6f24532d3e3c3e06
Nick Neal <nwneal@syr.edu>
2025-03-14 11:00:13 -0400

    test(proxy_test): check TestSiteExists

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -503,7 +503,7 @@ func TestSiteExists(t *testing.T) {
 	}{
 		{"test.local", true},
 		{"single.local", true},
-		{"fail.local", false},
+		{"fail.local", true},
 	}
 
 	os.Setenv("PROXYMANAGER_CONFIG_PATH", GetConfigPath())
